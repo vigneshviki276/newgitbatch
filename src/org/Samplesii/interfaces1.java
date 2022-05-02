0bae80ae295e01a97e4ded895dae53178049dd49
vignesviki276 <vigneshe707@gmail.com>
2022-05-02 16:32:52 +0530

    few lines of code is added here

---
+++ src/org/Samplesii/interfaces1.java
@@ -0,0 +1,28 @@
+
+package org. Samplesii;
+public  class interfaces1{
+
+
+
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		
+		try {
+		System.out.println(10/0);}
+						
+		catch(Throwable l){
+		System.out.println("dont/by zero");
+		}
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("end");
+		}
+		}
+
+
+
+

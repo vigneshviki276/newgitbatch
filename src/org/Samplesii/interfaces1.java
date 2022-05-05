692bfabf96218b1737e5cb6f8b5872b9f685040a
vignesviki276 <vigneshe707@gmail.com>
2022-05-06 00:16:50 +0530

    lines add

--- src/org/Samplesii/interfaces1.java
+++ src/org/Samplesii/interfaces1.java
@@ -2,26 +2,18 @@
 package org. Samplesii;
 public  class interfaces1{
 
-
+private void add() {
+System.out.println("print the order");
+}
 
 
 	public static void main(String[] args) {
-		System.out.println("start");
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("hello viki its created");
-		
-		try {
-		System.out.println(10/0);}
-						
-		catch(Throwable l){
-		System.out.println("dont/by zero");
-		}
-		System.out.println("4");
-		System.out.println("5");
-		System.out.println("end");
-		}
+		interfaces1 c= new interfaces1();
+	c.add();	
+	
+	
+	
+	}
 		}
 
 
